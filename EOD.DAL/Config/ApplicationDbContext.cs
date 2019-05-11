33740f9141f7edd88d1c644c6fb0093c2cc74cab
rummic <michal.rumianek@gmail.com>
2019-05-11 18:07:01 +0200

    Added dbcontext

---
+++ EOD.DAL/Config/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace EOD.DAL.Config
+{
+    using EOD.DAL.Model;
+    using System;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Case> Cases { get; set; }
+        public DbSet<Case> Departments { get; set; }
+        public DbSet<Case> Documents { get; set; }
+        public DbSet<Case> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(LocalDb)\MSSQLLocalDb;Database=EOD;Trusted_Connection=True;");   
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
