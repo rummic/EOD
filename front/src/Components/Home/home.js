351466a8c034712093447970ffb9c6481aa7adbe
Marcin Augusiewicz <stefek-zenonowaty@wp.pl>
2019-05-21 23:09:50 +0200

    add login component

--- front/src/Components/Home/home.js
+++ front/src/Components/Home/home.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button  from 'react-bootstrap/Button';
 
-
-function home() {
+class home extends Component {
+    constructor(props) {
+        super(props);
+        this.logout = this.logout.bind(this);
+    }
+    logout() {
+        sessionStorage.clear();    
+        this.props.history.push("/home")
+    }
+    render(){
   return (
     <div className="App">
       elo elo 
+      <Button variant="primary" size="lg" block href="/login">
+    login
+  </Button>
+      <Button onClick={this.logout} variant="primary"> Wyloguj</Button>
     </div>
-  );
-}
+  )
+}}
 
 export default home;
--- front/src/Components/Login/login.js
+++ front/src/Components/Login/login.js
@@ -1,12 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Form, Button, FormGroup } from 'react-bootstrap';
+import {Redirect} from 'react-router-dom';
 
+export function PostData(type,userData){
 
-function App() {
-  return (
-    <div className="App">
-      logowanie
-    </div>
-  );
+    let url = 'http://localhost:60148/api/Users/';
+
+    return new Promise((resolve,reject)=>{
+        fetch(url+type,{
+            method:'POST',
+            headers : { 
+                'Content-Type': 'application/json'
+               },
+            body: JSON.stringify(userData)
+        })
+        .then((response)=>response.json())
+        .then((responseJson)=>{
+            resolve(responseJson);
+        })
+        .catch((error)=>{
+            reject(error);
+        });
+    });
 }
 
-export default App;
+class login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: "",
+      password: "",
+    }
+    this.login = this.login.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+
+
+  login() {
+    if (this.state.login && this.state.password) {
+      PostData('Authenticate', this.state).then((result) => {
+        let responseJSON = result;
+        
+        if (responseJSON.value) {
+          sessionStorage.setItem('login',responseJSON.value.login);
+          sessionStorage.setItem('token',responseJSON.value.token);
+          sessionStorage.setItem('id',responseJSON.value.id);
+          this.props.history.push("/home")
+
+        } else {
+          document.getElementById("badLogin").innerHTML = "Błędne dane logowania";
+          document.getElementById("badLogin").style.color = "red";
+        }
+      });
+    }
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value })
+
+  }
+
+  render() {
+    if(sessionStorage.getItem("token")){
+      return(<Redirect to={'/home'}/>) 
+    }
+
+    return (
+      <div className="fomrconatiner">
+        <div className="formbox">
+
+          <Form.Group controlId="formBasicLogin">
+            <Form.Label>Login</Form.Label>
+            <Form.Control type="text" required placeholder="Login" name="login" onChange={this.onChange} />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassowrd">
+            <Form.Label>Hasło</Form.Label>
+            <Form.Control type="password" required placeholder="Hasło" name="password" onChange={this.onChange} />
+          </Form.Group>
+          <FormGroup>
+            <p id="badLogin"></p>
+          </FormGroup>
+          <Button variant="primary" type="submit" block onClick={this.login}>
+            Zaloguj
+                    </Button>
+
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default login;
