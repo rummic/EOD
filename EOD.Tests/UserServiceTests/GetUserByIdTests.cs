cb7cd426347053f31b100eef41654bdbeaa7a399
rummic <michal.rumianek@gmail.com>
2019-06-03 12:31:46 +0200

    Added additional test to GetUserByIdTests

--- EOD.Tests/UserServiceTests/GetUserByIdTests.cs
+++ EOD.Tests/UserServiceTests/GetUserByIdTests.cs
@@ -2,6 +2,7 @@
 {
     using System.Threading.Tasks;
 
+    using EOD.BL.Dtos.UserDtos;
     using EOD.BL.Services;
     using EOD.Commons.ErrorMessages;
     using EOD.DAL.Model;
@@ -19,12 +20,43 @@
         {
             var id = -1;
             User user = null;
-            _usersRepositoryMock.Setup(x => x.GetUserById(id, false)).ReturnsAsync(user);
+            _usersRepositoryMock.Setup(x => x.GetUserById(id, true)).ReturnsAsync(user);
 
             var service = new UsersService(_usersRepositoryMock.Object, _appSettingsMock.Object);
             var result = await service.GetUserById(id);
-            
+
             Assert.True(result.Errors.Contains(UserErrors.NotFoundById));
         }
+
+        [Test]
+        public async Task GetUserById_ValidId_ReturnsMappedUser()
+        {
+            var user = new User
+            {
+                Email = "test@test.com",
+                FirstName = "Test",
+                Id = 1,
+                IsDeleted = false,
+                LastName = "Testowy",
+                Login = "test",
+                Role = "User"
+            };
+            GetUserDto mappedUser = new GetUserDto
+            {
+                Email = "test@test.com",
+                Role = "User",
+                Id = 1,
+                IsDeleted = false,
+                LastName = "Testowy",
+                Login = "test",
+                FirstName = "Test"
+            };
+            var id = 1;
+            _usersRepositoryMock.Setup(x => x.GetUserById(id, true)).ReturnsAsync(user);
+            var service = new UsersService(_usersRepositoryMock.Object, _appSettingsMock.Object);
+            var result = await service.GetUserById(id);
+            Assert.True(mappedUser.AreEqual(result.Value));
+        }
+
     }
 }
