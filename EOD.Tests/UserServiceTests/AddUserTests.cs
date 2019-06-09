0990d6b2b74eb42b60230f5193c9c4dafe9fb575
Gatok <miko4500@gmail.com>
2019-06-09 21:06:45 +0200

    AddUserTests

--- EOD.Tests/UserServiceTests/AddUserTests.cs
+++ EOD.Tests/UserServiceTests/AddUserTests.cs
@@ -33,6 +33,74 @@ namespace EOD.Tests.UserServiceTests
         }
 
         [Test]
+        public async Task AddUser_EmptyFirstName_ReturnsEmptyFirstNameError()
+        {
+            AddUserDto userToDb = new AddUserDto
+            {
+                Login = "test",
+                Password = "password",
+                FirstName = "",
+                LastName = "Testowy",
+                Email = "test@test.com",
+                Role = "User"
+            };
+            var service = new UsersService(_usersRepositoryMock.Object, _appSettingsMock.Object);
+            var result = await service.AddUser(userToDb);
+            Assert.True(result.Errors.Contains(UserErrors.EmptyFirstName));
+        }
+
+        [Test]
+        public async Task AddUser_EmptyLastName_ReturnsEmptyLastNameError()
+        {
+            AddUserDto userToDb = new AddUserDto
+            {
+                Login = "test",
+                Password = "password",
+                FirstName = "Test",
+                LastName = "",
+                Email = "test@test.com",
+                Role = "User"
+            };
+            var service = new UsersService(_usersRepositoryMock.Object, _appSettingsMock.Object);
+            var result = await service.AddUser(userToDb);
+            Assert.True(result.Errors.Contains(UserErrors.EmptyLastName));
+        }
+
+        [Test]
+        public async Task AddUser_EmptyEmail_ReturnsEmptyEmailError()
+        {
+            AddUserDto userToDb = new AddUserDto
+            {
+                Login = "test",
+                Password = "password",
+                FirstName = "Test",
+                LastName = "Testowy",
+                Email = "",
+                Role = "User"
+            };
+            var service = new UsersService(_usersRepositoryMock.Object, _appSettingsMock.Object);
+            var result = await service.AddUser(userToDb);
+            Assert.True(result.Errors.Contains(UserErrors.EmptyEmail));
+        }
+
+        [Test]
+        public async Task AddUser_InvalidEmailFormat_ReturnsEmailInvalidError()
+        {
+            AddUserDto userToDb = new AddUserDto
+            {
+                Login = "test",
+                Password = "password",
+                FirstName = "Test",
+                LastName = "Testowy",
+                Email = "testtest",
+                Role = "User"
+            };
+            var service = new UsersService(_usersRepositoryMock.Object, _appSettingsMock.Object);
+            var result = await service.AddUser(userToDb);
+            Assert.True(result.Errors.Contains(UserErrors.EmailInvalid));
+        }
+
+        [Test]
         public async Task AddUser_ValidData_ReturnsNoErrors()
         {
             AddUserDto userToDb = new AddUserDto
